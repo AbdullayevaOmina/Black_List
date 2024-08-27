8d411a04a4d6ba5212fa3cf0d30c42e24e3781af
Omina Abdullayeva <ominaabdullayeva76@gmail.com>
2024-08-27 21:53:27 +0500

    ...

--- src/service/auth/index.ts
+++ src/service/auth/index.ts
@@ -1,6 +1,6 @@
 import request from "../config";
 import { Request } from "@auth-interface";
 export const auth: Request = {
-  signin: (data) => request.post("/login", data),
-  signup: (data) => request.post("/register", data),
+  signin: (data) => request.post("http://13.126.34.169:5555/login", data),
+  signup: (data) => request.post("http://13.126.34.169:5555/register", data),
 };
