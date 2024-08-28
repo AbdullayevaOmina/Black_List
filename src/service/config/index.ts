a19d553c322d12fc3691c2ff02b97939209c44b6
Omina Abdullayeva <ominaabdullayeva76@gmail.com>
2024-08-28 05:23:01 +0500

    ...

--- src/service/config/index.ts
+++ src/service/config/index.ts
@@ -1,11 +1,32 @@
+// import axios from "axios";
+// import { getDataFromCookie } from "@cookie";
+
+// const request = axios.create({
+//   baseURL:
+//     process.env.NODE_ENV === "production"
+//       ? "https://your-production-server.com" // HTTPS URL ni qo'llash
+//       : "http://13.126.34.169:5555", // Lokalhost uchun HTTP URL
+//   headers: {
+//     Authorization: `Bearer ${getDataFromCookie("access_token") || ""}`,
+//   },
+// });
+
+// request.interceptors.request.use((config) => {
+//   const access_token = getDataFromCookie("access_token");
+//   if (access_token) {
+//     config.headers["Authorization"] = `Bearer ${access_token}`;
+//   }
+//   return config;
+// });
+
+// export default request;
+
+
 import axios from "axios";
 import { getDataFromCookie } from "@cookie";
 
 const request = axios.create({
-  baseURL:
-    process.env.NODE_ENV === "production"
-      ? "https://your-production-server.com" // HTTPS URL ni qo'llash
-      : "http://13.126.34.169:5555", // Lokalhost uchun HTTP URL
+  baseURL: "http://13.126.34.169:5555", // HTTP URL ni qo'llash
   headers: {
     Authorization: `Bearer ${getDataFromCookie("access_token") || ""}`,
   },
