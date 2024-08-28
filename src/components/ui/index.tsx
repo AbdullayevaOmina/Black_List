334ec6896eb746bd2e37378aa0a012ffc3cad25b
Omina Abdullayeva <ominaabdullayeva76@gmail.com>
2024-08-28 12:12:58 +0500

    ...

--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -1,6 +1,13 @@
 import { MaskedInput } from "./maskedInput/MaskedInput";
 import { TableSkeleton } from "./skeleton";
 import GlobalSearch from "./search/index";
+import GlobalToast from "./toast/index";
 import { GlobalPagination } from "./pagination/Pagination";
 
-export { MaskedInput, TableSkeleton, GlobalSearch, GlobalPagination };
+export {
+  MaskedInput,
+  TableSkeleton,
+  GlobalSearch,
+  GlobalPagination,
+  GlobalToast,
+};
--- src/components/ui/toast/index.tsx
+++ src/components/ui/toast/index.tsx
@@ -1,19 +1,23 @@
-import { Toast } from 'flowbite-react';
+import { Toast } from "flowbite-react";
 import { HiFire } from "react-icons/hi";
 
-const GlobalToast = () => {
-	return (
+interface ToastProps {
+  text: string;
+  icon: string;
+}
+
+const GlobalToast = (props: ToastProps) => {
+  return (
     <>
-      <Toast>
+      <Toast className="absolute z-[1000] top-1 right-2 shadow-cyan-700 shadow-sm">
         <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-cyan-100 text-cyan-500 dark:bg-cyan-800 dark:text-cyan-200">
           <HiFire className="h-5 w-5" />
         </div>
-        <div className="ml-3 text-sm font-normal">Set yourself free.</div>
+        <div className="ml-3 text-sm font-normal">{props.text}</div>
         <Toast.Toggle />
       </Toast>
-      ;
     </>
   );
-}
+};
 
 export default GlobalToast;
